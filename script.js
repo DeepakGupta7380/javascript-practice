7cb5ee4b054ac16d3f4c43cebfc976441586a626
DeepakGupta7380 <sahudeepak7380.a@gmail.com>
2026-02-14 20:57:46 +0530

    0perator

---
+++ script.js
@@ -0,0 +1,25 @@
+let a = 10;
+let b = 5;
+
+console.log(a + b);
+console.log(a % b);
+
+let x = 10;
+x += 5
+console.log(x);
+
+console.log(5 == "5");
+console.log(5 === "5");
+
+//logical operator
+let age = 20;
+
+if(age > 18 && age < 30){
+    console.log("Young Adult");
+}
+
+//ternary operator
+let age1 = 18;
+let result = (age >= 18) ? "Adult" : "Minor";
+
+console.log(result);
