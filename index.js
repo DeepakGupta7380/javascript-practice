9b6c9525fb02eb2d775c628e7c146038cb315f6f
DeepakGupta7380 <sahudeepak7380.a@gmail.com>
2026-02-14 19:32:34 +0530

    update-1

---
+++ index.js
@@ -0,0 +1,72 @@
+
+//simple variable//
+var name="Deepak";
+console.log(name);
+
+
+//number store//
+var age = 25;
+console.log(age);
+
+
+//re-declare//
+var city = "Delhi";
+var city = "Mumbai";
+console.log(city);
+
+
+//re-assign//
+var price = 100;
+price = 200;
+
+console.log(price);
+
+
+//function scope//
+function test(){
+    var x = 10;
+    console.log(x);
+}
+test();
+
+//block var//
+if(true){
+    var number = 50;
+}
+console.log(number);
+
+//simple example//
+let name = "Deepak";
+console.log(name);
+
+//re-assign//
+let age = 20;
+age = 25;
+console.log(age);
+
+//re-declare//
+let city = "Delhi";
+let city = "Mumbai";
+
+//block scope//
+if(true){
+    let number = 100;
+    console.log(number);
+}
+console.log(number);
+
+//simple example//
+const name = "Deepak"
+console.log(name);
+
+//re-assign//
+const age = 25;
+age = 30;
+
+//block scope//
+if(true){
+    const number = 100;
+    console.log(number);
+}
+
+console.log(number);
